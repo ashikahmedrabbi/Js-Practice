8f04240da8989cc61a2701dac95039def2597ee8
unknown <ashikahmedrabbi@gmail.com>
2024-08-19 12:12:06 +0600

    19.08.24

---
+++ problem1.js
@@ -0,0 +1,59 @@
+// < 
+// > 
+
+// == 
+// != not equal 
+// <= 
+// >= 
+
+// &&  and
+
+// || or
+
+
+
+// var x=1000;
+// var y=1000;
+
+// if(x==y){
+//     console.log("Equal,a==b");
+// }else if (x>y)  {
+//     console.log("x is greater than y");
+// }else{
+//     console.log("x is less than y");
+// }
+
+// if(x!=y){
+//     console.log("dhar nib");
+// }else{
+//     console.log("dhar nib na");
+// }
+
+
+
+// var gender ="male";
+
+// var dele= "chittagong";
+
+// if (gender=="Female" && dele == "dhaka"){
+//     console.log("Free Delivery");
+// }else if (gender=="Female" || dele == "chittagong"){
+//     console.log("50% discount");
+
+// }else{
+//     console.log("No Discount");
+// }
+
+
+// var x =100;
+// var y=200;
+// var z=30;
+
+// if(x>y && x>z){
+//     console.log ("x is greater");
+// }else if(y>x && y>z){
+//     console.log("y is greater");
+// }else{
+//     console.log("z is greater");
+// }
+
